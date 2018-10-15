e23a462d35233ed2ba2ee076153216f549aff951
Simen Tennøe <simetenn@gmail.com>
2018-10-15 15:04:12 +0200

    Added bursting, spiking and aps features

--- medaka_model/burstiness.py
+++ medaka_model/burstiness.py
@@ -45,14 +45,14 @@ def duration(time, voltage):
 
 
 
-def burstiness(event_durations, burst_threshold=60):
+def burstiness(durations, burst_threshold=60):
     """
     Calculate the burstiness from a series of event durations by finding the
     fraction of events that is above `burst_threshold`.
 
     Parameters
     ----------
-    event_durations : array
+    durations : array
         A numpy array containing the duration of each spike in `voltage`.
     burst_threshold : float, optional
         Default is 70 ms.
@@ -63,11 +63,14 @@ def burstiness(event_durations, burst_threshold=60):
         The fraction of events that is above `burst_threshold`. Returns None
         if there are no events.
     """
-    if not event_durations.size:
+
+    durations = np.array(durations)
+
+    if not durations.size:
         return None
 
     # Find all events greater than the burst_threshold
-    burst_occurrences = event_durations > burst_threshold
+    burst_occurrences = durations > burst_threshold
 
     # Calculate the fraction of these events
     burstiness_factor = np.sum(burst_occurrences)/len(burst_occurrences)
@@ -97,28 +100,27 @@ def burstiness_factor(time, spikes, info):
         The fraction of events that is above `burst_threshold`. Returns None
         if there are no events.
     """
-    voltage = spikes.V
-    time = spikes.time
+    # Calculate the duration of each spike
+    durations = []
+    for spike in spikes:
+        durations.append(spike.time[-1] - spike.time[0])
 
-    event_durations = duration(time, voltage)
-
-    burstiness_factor = burstiness(event_durations, burst_threshold=burst_threshold)
+    burstiness_factor = burstiness(durations, burst_threshold=burst_threshold)
 
     return None, burstiness_factor
 
 
 
-def burstiness_efel(time, voltage, info):
+def bursting(time, spikes, info):
     """
-    Calculate the burstiness factor from a uncertainpy.Spikes object.
-    Compatible with uncertainpy.EfelFeatures.
+    Compatible with uncertainpy.SpikingFeatures.
 
     Parameters
     ----------
-    time : array_like
-        Time array, in ms.
-    voltage : array_like
-        Voltage array from a neuron.
+    time : {None, numpy.nan, array_like}
+        Time values of the model. If no time values it is None or numpy.nan.
+    spikes : uncertainpy.Spikes
+        Spikes found in the model result.
     info : dictionary
         Not used, but is required as input
 
@@ -129,8 +131,126 @@ def burstiness_efel(time, voltage, info):
         The fraction of events that is above `burst_threshold`. Returns None
         if there are no events.
     """
-    event_durations = duration(time, voltage)
+    for spike in spikes:
+        if (spike.time[-1] - spike.time[0]) > burst_threshold:
+            return None, 1
 
-    burstiness_factor = burstiness(event_durations, burst_threshold=burst_threshold)
+    return None, 0
 
-    return None, burstiness_factor
+
+def spiking(time, spikes, info):
+    """
+    Compatible with uncertainpy.SpikingFeatures.
+
+    Parameters
+    ----------
+    time : {None, numpy.nan, array_like}
+        Time values of the model. If no time values it is None or numpy.nan.
+    spikes : uncertainpy.Spikes
+        Spikes found in the model result.
+    info : dictionary
+        Not used, but is required as input
+
+    Returns
+    -------
+    time : None
+    burstiness_factor : float, None
+        The fraction of events that is above `burst_threshold`. Returns None
+        if there are no events.
+    """
+    for spike in spikes:
+        if (spike.time[-1] - spike.time[0]) < burst_threshold:
+            return None, 1
+
+    return None, 0
+
+
+def APs(time, spikes, info):
+    """
+    Compatible with uncertainpy.SpikingFeatures.
+
+    Parameters
+    ----------
+    time : {None, numpy.nan, array_like}
+        Time values of the model. If no time values it is None or numpy.nan.
+    spikes : uncertainpy.Spikes
+        Spikes found in the model result.
+    info : dictionary
+        Not used, but is required as input
+
+    Returns
+    -------
+    time : None
+    burstiness_factor : float, None
+        The fraction of events that is above `burst_threshold`. Returns None
+        if there are no events.
+    """
+    APs = 0
+    if spikes.nr_spikes > 0:
+        APs = 1
+
+    return None, APs
+
+
+
+def bursts_spikes_aps(time, spikes, info):
+    """
+    Compatible with uncertainpy.SpikingFeatures.
+
+    Parameters
+    ----------
+    time : {None, numpy.nan, array_like}
+        Time values of the model. If no time values it is None or numpy.nan.
+    spikes : uncertainpy.Spikes
+        Spikes found in the model result.
+    info : dictionary
+        Not used, but is required as input
+
+    Returns
+    -------
+    time : None
+    burstiness_factor : float, None
+        The fraction of events that is above `burst_threshold`. Returns None
+        if there are no events.
+    """
+    voltage = spikes.V
+    time = spikes.time
+
+    durations = duration(time, voltage)
+
+    if not durations.size:
+        bursts = -1
+    elif np.any(durations > burst_threshold):
+        bursts = 1
+    else:
+        bursts = 0
+
+
+    return None, bursts
+
+# def burstiness_efel(time, voltage, info):
+#     """
+#     Calculate the burstiness factor from a uncertainpy.Spikes object.
+#     Compatible with uncertainpy.EfelFeatures.
+
+#     Parameters
+#     ----------
+#     time : array_like
+#         Time array, in ms.
+#     voltage : array_like
+#         Voltage array from a neuron.
+#     info : dictionary
+#         Not used, but is required as input
+
+#     Returns
+#     -------
+#     time : None
+#     burstiness_factor : float, None
+#         The fraction of events that is above `burst_threshold`. Returns None
+#         if there are no events.
+#     """
+#     event_durations = duration(time, voltage)
+
+#     burstiness_factor = burstiness(event_durations, burst_threshold=burst_threshold)
+
+#     return None, burstiness_factor
