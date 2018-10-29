4cf36fab8f2c657990be05f49f2ef482e7ee40f2
Simen Tennøe <simetenn@gmail.com>
2018-10-29 15:00:21 +0100

    Dockerfile

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -eo pipefail
 
-if [ $# -eq 0 ]
-    then
-        echo "Python 2 or 3 must be specified"
-        exit 1
-fi
-
-docker build -t python_medaka$1 -f .docker/Dockerfile_python$1 .
-docker build -t uncertainpy_medaka$1 -f .docker/Dockerfile_uncertainpy$1 .
-docker run -i -v $(pwd):/home/docker/tabak -t uncertainpy_medaka$1
+docker build -t python_tabak -f Dockerfile .
+docker run -i -v $(pwd):/home/docker/tabak -t python_tabak
